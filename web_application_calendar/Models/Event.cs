5c115782bdb70920e49deb02e5b9a8b4457fd9db
Garwin Wong <garwin.wong@gmail.com>
2019-03-19 21:04:33 +0000

    Initial

---
+++ web_application_calendar/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace web_application_calendar.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime Start { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime End { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ web_application_calendar/Models/web_application_calendarContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace web_application_calendar.Models
+{
+    public class web_application_calendarContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public web_application_calendarContext() : base("name=web_application_calendarContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<web_application_calendar.Models.Event> Events { get; set; }
+    }
+}
